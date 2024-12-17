231fe64a293b823f39cff8d84ae7e5953ced489d
diego <diegitsen@gmail.com>
2024-12-17 21:51:43 +0100

    add carousel and detail

--- idealistaChallenge/ui/listOfProperties/ListViewController.swift
+++ idealistaChallenge/ui/listOfProperties/ListViewController.swift
@@ -254,14 +254,7 @@ class ListViewController: UIViewController, MainProtocol {
     func setupData() {
         getListOfProperties()
     }
-    
-    func setupTableView() {
-        listTableView.dataSource = self
-        listTableView.delegate = self
-        listTableView.register(PropertyCell.self, forCellReuseIdentifier: PropertyCell.cellID)
-        listTableView.separatorStyle = .none
-        listTableView.rowHeight = UITableView.automaticDimension
-    }
+
     
     private func setupObservers() {
   
@@ -360,31 +353,3 @@ class ListViewController: UIViewController, MainProtocol {
 //        }
     }
 }
-
-extension ListViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.properties.value?.count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: PropertyCell.cellID) as! PropertyCell
-        guard let properties = viewModel.properties.value else {
-            return cell
-        }
-        let property = properties[indexPath.row]
-        cell.selectionStyle = .none
-        cell.setupCell(property: property)
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let property = viewModel.properties.value?[indexPath.row] else {
-            return
-        }
-        //TODO: create a router
-//        let vc = PerformanceDetailViewController()
-//        vc.performance = performance
-//        self.navigationController?.pushViewController(vc, animated: true)
-    }
-}
-
